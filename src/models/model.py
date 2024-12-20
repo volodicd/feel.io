18eb9412fb6bb38ca9893bfe44899bc3e1f3dcb1
volodicd <volodiaint@gmail.com>
2024-12-20 03:53:29 +0100

    optimize model

--- src/models/model.py
+++ src/models/model.py
@@ -33,43 +33,27 @@ class ImprovedEmotionModel(nn.Module):
         # 1. Image Encoder
         # -------------------------------------------------------------
         self.image_encoder = nn.Sequential (
-            # Initial feature extraction
-            nn.Conv2d (3, 64, kernel_size=5, stride=1, padding=2),
+            # First block
+            nn.Conv2d (3, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d (64),
             nn.ReLU (),
             nn.MaxPool2d (2),
-            nn.Dropout (0.2),
 
-            # First block - increase channels
+            # Second block
             nn.Conv2d (64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d (128),
             nn.ReLU (),
-            nn.Conv2d (128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d (128),
-            nn.ReLU (),
             nn.MaxPool2d (2),
-            nn.Dropout (0.3),
 
-            # Second block - extract emotion features
+            # Third block
             nn.Conv2d (128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d (256),
-            nn.ReLU (),
-            nn.Conv2d (256, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d (256),
-            nn.ReLU (),
-            nn.MaxPool2d (2),
-            nn.Dropout (0.4),
-
-            # Global features
-            nn.Conv2d (256, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d (512),
             nn.ReLU (),
             nn.MaxPool2d (2),
 
             nn.AdaptiveAvgPool2d ((1, 1)),
             nn.Flatten (),
-            nn.Linear (512, 256),
-            nn.BatchNorm1d (256),
+            nn.Linear (256, 256),
             nn.ReLU (),
             nn.Dropout (0.5)
         )
